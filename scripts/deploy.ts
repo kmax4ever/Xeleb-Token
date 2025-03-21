c8b2a1483197f10c00ec861fe042084c0fce5381
kmax3ever <kmax3ever2@gmail.com>
2025-03-21 23:22:32 +0700

    add case deploy testnet bsc

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,9 @@ import { ethers } from "hardhat";
 const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT);
 import * as config from "./config.json";
 import {
+  delay,
   etherProvider,
+  fromWei,
   getDetails,
   getTokensForETH,
   getWalletFromPkey,
@@ -10,6 +12,8 @@ import {
 } from "./utils";
 
 // case 1 test balance vesting
+
+import * as FactoryJson from "../artifacts/contracts/Controller.sol/Controller.json";
 
 async function main() {
   const pkeyAdmin = process.env.ADMIN_KEY;
@@ -21,44 +25,54 @@ async function main() {
   const creator = getWalletFromPkey(process.env.CREATOR_KEY);
   const newToken = config.NEW_TOKEN;
 
-  console.log("-------CREATE TOKEN------");
-  const tx = await factory
-    .connect(creator)
-    .createToken(
-      newToken.name,
-      newToken.symbol,
-      config.WALLET.STAKING,
-      toWei(newToken.totalSupply),
-      { value: toWei(0.3) }
+  if (Number(process.env.CHAIN_ID) !== 97) {
+    console.log("-------CREATE TOKEN------");
+    const tx = await factory
+      .connect(creator)
+      .createToken(
+        newToken.name,
+        newToken.symbol,
+        config.WALLET.STAKING,
+        toWei(newToken.totalSupply),
+        { value: toWei(0.4) }
+      );
+
+    await tx.wait();
+    console.log("hash :", tx.hash);
+    const agentTokenAddr = await factory.getTokenByOwner(creator.address);
+    const bondingAddr = await factory.getBondingByToken(agentTokenAddr);
+    const bodingContract = await ethers.getContractAt(
+      "BondingCurve",
+      bondingAddr
     );
 
-  await tx.wait();
+    //await getDetails(agentTokenAddr, creator.address, "CREATOR");
 
-  console.log("hash :", tx.hash);
-
-  const agentTokenAddr = await factory.getTokenByOwner(creator.address);
-  const bondingAddr = await factory.getBondingByToken(agentTokenAddr);
-  const bodingContract = await ethers.getContractAt(
-    "BondingCurve",
-    bondingAddr
-  );
-  // const weth = await bodingContract.weth2();
-  // console.log({ weth });
-
-  //await getDetails(agentTokenAddr, creator.address, "CREATOR");
-
-  const CONTRACT_ADDRESSES_MAP = {
-    controller: await factory.getAddress(),
-    agentTokenAddr,
-    bondingAddr,
-    chainID: process.env.CHAIN_ID,
-    rpc: process.env.RPC_ENDPOINT,
-  };
-  var fs = require("fs");
-  fs.writeFileSync(
-    "./scripts/contracts.json",
-    JSON.stringify(CONTRACT_ADDRESSES_MAP, null, 4)
-  );
+    const CONTRACT_ADDRESSES_MAP = {
+      controller: await factory.getAddress(),
+      agentTokenAddr,
+      bondingAddr,
+      chainID: process.env.CHAIN_ID,
+      rpc: process.env.RPC_ENDPOINT,
+    };
+    var fs = require("fs");
+    fs.writeFileSync(
+      "./scripts/contracts.json",
+      JSON.stringify(CONTRACT_ADDRESSES_MAP, null, 4)
+    );
+  } else {
+    console.log("DEPLOY factory");
+    const CONTRACT_ADDRESSES_MAP = {
+      controller: await factory.getAddress(),
+      chainID: process.env.CHAIN_ID,
+      rpc: process.env.RPC_ENDPOINT,
+    };
+    var fs = require("fs");
+    fs.writeFileSync(
+      "./scripts/contracts.json",
+      JSON.stringify(CONTRACT_ADDRESSES_MAP, null, 4)
+    );
+  }
 }
 
 main().catch((error) => {
